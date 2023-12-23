25072428b9da1904e7a168bf1781a4518b15cdc5
Oaní da Silva da Costa <oani_costa@outlook.com.br>
2023-12-22 23:13:40 -0300

    .....

--- Solucao/Pages/Details.cshtml.cs
+++ Solucao/Pages/Details.cshtml.cs
@@ -8,23 +8,28 @@ namespace Solucao.Pages
     {
         private readonly DbContext _context;
 
-        public DetailsModel(DbContext context)
+        public DetailsModel()
         {
-            _context = context;
+            _context = new DbContext();
         }
 
-        public Product Product { get; set; }
+        public Produto Product { get; set; }
 
         public void OnGet(int id)
         {
-            string sqlQuery = $"SELECT Name, Quantity FROM Products WHERE Id = {id}";
+            string sqlQuery = $"SELECT produtoid, nome, descrição, valorunitario , quantidade FROM estoque E JOIN produto p ON  e.produtoid = p.id WHERE produtoid = {id}";
+            //produtoid nome    descrição valorunitario   quantidade
+
             DataTable dataTable = _context.ExecuteQuery(sqlQuery);
             if (dataTable.Rows.Count > 0)
             {
                 DataRow row = dataTable.Rows[0];
-                string name = Convert.ToString(row["Name"]);
-                int quantity = Convert.ToInt32(row["Quantity"]);
-                Product = new Product(id, quantity, name);
+
+                string name = Convert.ToString(row["nome"]);
+                string descricao = Convert.ToString(row["descrição"]);
+                int quantity = Convert.ToInt32(row["quantidade"]);
+                double valUnit = Convert.ToDouble(row["valorunitario"]);
+                Product = new Produto(id, quantity, valUnit, descricao, name);
             }
         }
     }
--- Solucao/Pages/Index.cshtml.cs
+++ Solucao/Pages/Index.cshtml.cs
@@ -8,23 +8,27 @@ namespace Solucao.Pages
     {
         private readonly DbContext _context;
 
-        public IndexModel(DbContext context)
+        public IndexModel()
         {
-            _context = context;
+            _context = new DbContext();
         }
 
-        public List<Product> Products { get; set; }
+        public List<Produto> Products { get; set; }
 
         public void OnGet()
         {
-            DataTable dataTable = _context.ExecuteQuery("SELECT * FROM Products");
-            Products = new List<Product>();
+            DataTable dataTable = _context.ExecuteQuery("SELECT produtoid, nome, descrição, valorunitario , quantidade FROM estoque E JOIN produto p ON  e.produtoid = p.id");
+            //produtoid nome    descrição valorunitario   quantidade
+
+            Products = new List<Produto>();
             foreach (DataRow row in dataTable.Rows)
             {
-                int id = Convert.ToInt32(row["Id"]);
-                int quantity = Convert.ToInt32(row["Quantity"]);
-                string name = Convert.ToString(row["Name"]);
-                Products.Add(new Product(id, quantity, name));
+                int id = Convert.ToInt32(row["produtoid"]);
+                int quantity = Convert.ToInt32(row["quantidade"]);
+                string name = Convert.ToString(row["nome"]);
+                string dscr = Convert.ToString(row["descrição"]);
+                double valUnit = Convert.ToDouble(row["valorunitario"]);
+                Products.Add(new Produto(id, quantity, valUnit, dscr, name));
             }
         }
     }
